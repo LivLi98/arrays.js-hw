5e2351dc764ef7e12a558bc0b7d24e8ffeb69718
Olivia Anderson <liv.and1012@gmail.com>
2023-03-15 09:18:51 -0400

    first commit

---
+++ array.js
@@ -0,0 +1,43 @@
+
+// let planets =['Eartth','Jupiter','Neptune','Mars','Saturn','Mercury','Uranus','Venus']
+// console.log(planets);
+
+// let planets =['Eartth','Jupiter','Neptune','Mars','Saturn','Mercury','Uranus','Venus']
+// console.log(planets[3])
+
+// let planets =['Eartth','Jupiter','Neptune','Mars','Saturn','Mercury','Uranus','Venus']
+// console.log(planets[4])
+
+
+// let planets =['Eartth','Jupiter','Neptune','Mars','Saturn','Mercury','Uranus','Venus']
+// console.log(planets[7])
+
+// let planets =['Eartth','Jupiter','Neptune','Mars','Saturn','Mercury','Uranus','Venus']
+// for( let index=0; index<planets.length; index++){
+//     console.log(index)
+// }
+
+// let planets =['Earth','Jupiter','Neptune','Mars','Saturn','Mercury','Uranus','Venus']
+// for(let i=0;i<planets.length; i++){
+//     console.log(planets[i])            
+// }
+// 8. Create a while loop that iterates through each of the items in the `planet` array
+// and changes each element to lowercase.
+
+// let planets =['Earth','Jupiter','Neptune','Mars','Saturn','Mercury','Uranus','Venus']
+// let i=0
+// while(i<planets.length){
+//     console.log(planets[i].toLowerCase());
+//     i++
+// }
+
+// 9. Add **Pluto** to the `planet` array.
+
+// let planets =['Eartth','Jupiter','Neptune','Mars','Saturn','Mercury','Uranus','Venus']
+// console.log(planets.push('Pluto'));
+// console.log(planets)
+    
+
+// // 10. Loop through each planet in the `planet` array and print them on the command line.
+// let planets =['Earth','Jupiter','Neptune','Mars','Saturn','Mercury','Uranus','Venus']
+// let i=0
